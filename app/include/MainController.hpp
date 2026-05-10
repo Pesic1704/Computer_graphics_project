ffcd434e02f32e1860dc10bee6c088b4cd1f5f7b
Pesic1704 <petar.pesic04@gmail.com>
2026-05-10 17:33:34 +0200

    feat: implement basic controller functions

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -5,7 +5,23 @@
 
 namespace my_app {
     class MainController final : public engine::core::Controller {
+    public:
         [[nodiscard]] std::string_view name() const override;
+
+    private:
+        void initialize() override;
+
+        bool loop() override;
+
+        void poll_events() override;
+
+        void update() override;
+
+        void begin_draw() override;
+
+        void draw() override;
+
+        void end_draw() override;
     };
 }
 
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,4 +4,36 @@ namespace my_app {
     std::string_view MainController::name() const {
         return "MainController";
     }
+
+    void MainController::initialize() {
+        engine::graphics::OpenGL::enable_depth_testing();
+    }
+
+    bool MainController::loop() {
+        auto platform = get<engine::platform::PlatformController>();
+
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).state() == engine::platform::Key::State::Pressed) {
+            return false;
+        }
+
+        return true;
+    }
+
+    void MainController::poll_events() {
+    }
+
+    void MainController::update() {
+    }
+
+    void MainController::begin_draw() {
+        engine::graphics::OpenGL::clear_buffers();
+    }
+
+    void MainController::draw() {
+    }
+
+    void MainController::end_draw() {
+        auto platform = get<engine::platform::PlatformController>();
+        platform->swap_buffers();
+    }
 }
